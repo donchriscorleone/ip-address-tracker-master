bf5be009b724ab89cc74bb2c0624a2e84a5bd810
Christopher II Lajom <christopheriilajom@gmail.com>
2022-11-09 11:01:42 +0800

    Modified.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const baseApi = `https://geo.ipify.org/api/v2/country,city?apiKey=${"at_04riVppoAWt4NcDU4dwB266oQabhl"}`
+const baseApi = `https://geo.ipify.org/api/v2/country,city?apiKey=${process.env.MY_API_KEY}`
 const headers = {
     method: 'GET',
     headers: { 
